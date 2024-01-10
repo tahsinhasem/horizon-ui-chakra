b732e71182f34a2233397621bd744014e895d65f
Tahsin <tahsinhasem@gmail.com>
2024-01-11 01:41:28 +0600

    Modified ip to 3.85.4.6:8000

--- src/utils/useAxios.js
+++ src/utils/useAxios.js
@@ -6,7 +6,7 @@ import { jwtDecode } from "jwt-decode";
 import dayjs from "dayjs";
 
 
-const baseUrl = "http://127.0.0.1:8000"
+const baseUrl = "http://3.85.4.6:8000"
 
 // TODO: Handle refresh token expiry
 const useAxios = () => {
